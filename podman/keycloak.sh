1a655795e8a4a91e5797b5982b40b1b30e75cd59
Joseph King <joking164@icloud.com>
2025-02-06 19:55:43 -0500

    Podman working for keycloak added template files for other services

---
+++ podman/keycloak.sh
@@ -0,0 +1,28 @@
+podman network create keycloak
+
+podman volume create keycloak-data
+
+podman run -d --network keycloak --name keycloakdb \
+    -v keycloak-data:/var/lib/postgresql/data \
+    -e POSTGRES_DB=keycloak \
+    -e POSTGRES_USER=keycloak \
+    -e POSTGRES_PASSWORD=password \
+    postgres:15
+
+podman run -d --network keycloak --name keycloak -p 8080:8080 \
+    -e KC_DB=postgres \
+    -e KC_DB_URL=jdbc:postgresql://keycloakdb:5432/keycloak \
+    -e KC_DB_USERNAME=keycloak \
+    -e KC_DB_PASSWORD=password \
+    -e KC_HOSTNAME=localhost \
+    -e KC_HOSTNAME_PORT=8080 \
+    -e KC_HOSTNAME_STRICT=false \
+    -e KC_HOSTNAME_STRICT_HTTPS=false \
+    -e KC_HTTP_ENABLED=true \
+    -e KC_PROXY=edge \
+    -e KC_LOG_LEVEL=info \
+    -e KC_METRICS_ENABLED=true \
+    -e KC_HEALTH_ENABLED=true \
+    -e KEYCLOAK_ADMIN=admin \
+    -e KEYCLOAK_ADMIN_PASSWORD=admin \
+    quay.io/keycloak/keycloak:latest start-dev
